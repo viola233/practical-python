21e3b3ad4ee8cb0d02443fa94babae6e524d7a7e
viola233 <jingyaol@mun.ca>
2021-06-05 13:42:02 -0230

    add exercise1.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,15 @@
 # pcost.py
 #
 # Exercise 1.27
+f = open('Data/portfolio.csv', 'rt')
+headers = next(f).split(',')
+
+total = 0
+
+for line in f:
+    row = line.split(',')
+    total += int(row[1]) * float(row[2])
+
+f.close()
+
+print(total)
